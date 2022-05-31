047088e060daf42b45378b883404dda724eadd97
kolson <kolson@dmsi.com>
2022-05-31 15:57:40 -0500

    allow cors

--- server/server.go
+++ server/server.go
@@ -6,20 +6,30 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
 	"github.com/kolson4282/bible-collection/biblecollection"
 	"github.com/kolson4282/bible-collection/graph/generated"
 	"github.com/kolson4282/bible-collection/graph/resolver"
+	"github.com/rs/cors"
 )
 
 func StartServer(collection biblecollection.BibleCollection, port string) {
+
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{
 		Collection: collection,
 	}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
